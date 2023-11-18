a37949e968a0ba2b5bb28fee127cf40ee383e689
Tbot223 <104832561+Tbot223@users.noreply.github.com>
2023-11-18 23:45:21 +0900

    update 002, test.cpp

--- 002/test.cpp
+++ 002/test.cpp
@@ -8,25 +8,26 @@ list<string> strings;
 int main()
 {
     cout << "~을 입력해주세요...\n['END'를 입력할시 종료됩니다.]\n\n";
-    while (true)
+    for (;;)
     {
         string input;
         cin >> input;
-        if ("end" != input || "END" != input)
-        {
-            strings.push_back(input);
-            continue;
-        }else if ("end" == input || "END" == input)
+        if ("end" == input || "END" == input)
         {
             int n = strings.size();
             list<string>::iterator it = strings.begin();
+            cout << "\n\n저장된 입력값들 : ";
             for (int i = 0; i < n; i++)
             {
                 string val = *it;
-                cout << "\n\n" << val << " ";
+                cout << val << ", ";
                 it++;
             }
             break;
+        }else
+        {
+            strings.push_back(input);
+            continue;
         }
     }
 }
